361bc1f4e47230365f7fa4424bc6388e73bc8972
Ubuntu <ubuntu@ip-172-31-8-104.ap-northeast-2.compute.internal>
2017-10-27 18:04:50 +0000

    add food model

---
+++ src/models/food.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String
+import src.orm as orm
+
+Base = orm.Base
+
+class Food(Base):
+    __tablename__ = "food"
+    
+    name = Column(String(20), primary_key=True)
+    kcal = Column(Integer)
+    co2 = Column(Integer)
+   
+    def __init__(self, name, kcal, co2):
+        self.name = name
+        self.kcal = kcal
+        self.co2 = co2
+
+    def as_dict(self):
+        return {
+                    "name" : self.name,
+                    "kcal" : self.kcal,
+                    "co2" : self.co2,
+               }
