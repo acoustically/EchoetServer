d0619514eba30ff9828a30f8fd23b2cc521f73ca
Ubuntu <ubuntu@ip-172-31-8-104.ap-northeast-2.compute.internal>
2017-10-27 18:55:33 +0000

    modify daily eat model column, food to food_name

--- src/models/daily_eat.py
+++ src/models/daily_eat.py
@@ -7,15 +7,15 @@ class DailyEat(Base):
     __tablename__ = "daily_eats"
     
     user_id = Column(String(20), primary_key=True)
-    food = Column(String(50))
+    food_name = Column(String(50))
     year = Column(Integer)
     month = Column(Integer)
     date = Column(Integer)
     count = Column(Integer, default=1) 
    
-    def __init__(self, user_id, food, year, month, date):
+    def __init__(self, user_id, food_name, year, month, date):
         self.user_id = user_id
-        self.food = food
+        self.food_name = food_name
         self.year = year
         self.month = month
         self.date = date
@@ -23,7 +23,7 @@ class DailyEat(Base):
     def as_dict(self):
         return {
                     "user_id" : self.user_id,
-                    "food" : self.food,
+                    "food_name" : self.food_name,
                     "year" : self.year,
                     "month" : self.month,
                     "date" : self.date,
